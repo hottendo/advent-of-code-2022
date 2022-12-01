71172e03953e0384003687047a0f8a0b7b9cfd27
Thomas Hottendorff <thomas.hottendorff@hvbo4.de>
2022-12-01 10:59:41 +0100

    Solution for day 01.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,49 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var mostCalories = 0
+
+        var sumCalories = 0
+        for (item in input) {
+            if (item.length == 0) {
+                if (sumCalories > mostCalories) {
+                    mostCalories = sumCalories
+                }
+                sumCalories = 0
+                // println("Maximum calories: $mostCalories")
+            } else {
+                sumCalories = sumCalories + item.toInt()
+                // println("Sum of calories: $sumCalories")
+            }
+        }
+        return mostCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val topThreeMostCalories: IntArray = intArrayOf(0, 0, 0)
+        var sumCalories = 0
+
+        for (item in input) if (item.length == 0) {
+            if (sumCalories > topThreeMostCalories[0]) {
+                topThreeMostCalories[0] = sumCalories
+                topThreeMostCalories.sort()
+                for(cal in topThreeMostCalories) {
+                    print("$cal ")
+                }
+                println()
+            }
+            sumCalories = 0
+        } else {
+            sumCalories += item.toInt()
+        }
+        return topThreeMostCalories.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    println(part2(testInput))
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
